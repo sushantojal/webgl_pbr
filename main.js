4a0d8cdc50d856cc70d888bd5c482da02a763e55
Sushant Ojal <sushantojal@gmail.com>
2018-12-31 00:55:13 -0800

    fragment shader wip

--- main.js
+++ main.js
@@ -37,8 +37,121 @@ function main() {
   // Fragment shader program
 
   const fsSource = `
+
+    //specular components
+
+    //fresnel/specular fraction (or the fraction of light that gets reflected)
+    vec3 fresnelSchlick(float cosTheta, vec3 F0)
+    {
+      return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
+    }
+
+    //normal distribution, depends on roughness of the material.
+    //also, proportional to the alignment of microfacets to the half vector
+    float DistributionGGX(vec3 N, vec3 H, float roughness)
+    {
+        float a      = roughness*roughness;
+        float a2     = a*a;
+        float NdotH  = max(dot(N, H), 0.0);
+        float NdotH2 = NdotH*NdotH;
+
+        float num   = a2;
+        float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+        denom = PI * denom * denom;
+
+        return num / denom;
+    }
+
+    //self occlusion due to the roughness of the material
+    float GeometrySchlickGGX(float NdotV, float roughness)
+    {
+        float r = (roughness + 1.0);
+        float k = (r*r) / 8.0;
+
+        float num   = NdotV;
+        float denom = NdotV * (1.0 - k) + k;
+
+        return num / denom;
+    }
+    float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+    {
+        float NdotV = max(dot(N, V), 0.0);
+        float NdotL = max(dot(N, L), 0.0);
+        float ggx2  = GeometrySchlickGGX(NdotV, roughness);
+        float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+
+        return ggx1 * ggx2;
+    }
+
+
+    // in vec2 TexCoords;
+    in vec3 WorldPos;
+    in vec3 Normal;
+
+    uniform vec3 camPos;
+
+    uniform vec3  albedo;
+    uniform float metallic;
+    uniform float roughness;
+    uniform float ao;
+
+    uniform vec3 lightPosition;
+    uniform vec3 lightColor;
+
     void main(void) {
-      gl_FragColor = vec4(1.0,0.0,0,1);
+
+      vec3 N = normalize(Normal);
+      vec3 V = normalize(camPos - WorldPos);
+
+      vec3 Lo = vec3(0.0);
+
+      //iterate for multiple lights
+      for(int i = 0; i < 1; ++i)
+      {
+        vec3 L = normalize(lightPositions[i] - WorldPos);
+        vec3 H = normalize(V + L);
+        float distance    = length(lightPositions[i] - WorldPos);
+        float attenuation = 1.0 / (distance * distance);
+        vec3 radiance     = lightColors[i] * attenuation;
+
+
+        //find three components responsible for specular output
+
+        //fresnel
+        //for dielectrics low value of base refelectivity
+        vec3 F0 = vec3(0.04);
+        F0      = mix(F0, albedo, metallic);
+        vec3 F  = fresnelSchlick(max(dot(H, V), 0.0), F0);
+
+        //normal distribution
+        float NDF = DistributionGGX(N, H, roughness);
+
+        //geometric occlusion
+        float G   = GeometrySmith(N, V, L, roughness);
+
+        vec3 numerator    = NDF * G * F;
+        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
+        vec3 specular     = numerator / max(denominator, 0.001);
+
+        vec3 kS = F;
+        vec3 kD = vec3(1.0) - kS;
+
+        kD *= 1.0 - metallic;
+
+        const float PI = 3.14159265359;
+
+        float NdotL = max(dot(N, L), 0.0);
+        Lo += (kD * albedo / PI + specular) * radiance * NdotL;
+      }
+
+      vec3 ambient = vec3(0.03) * albedo * ao;
+      vec3 color = ambient + Lo;
+
+      color = color / (color + vec3(1.0));
+      color = pow(color, vec3(1.0/2.2));
+
+      gl_FragColor = vec4(color, 1.0);
+
     }
   `;
 
